592a7370a95b236cf6eda3238af369ab021bdaab
Rustam <mirzamorf@yandex.ru>
2020-05-15 21:42:04 +0300

    added new api method

--- src/services/FirebaseService.js
+++ src/services/FirebaseService.js
@@ -4,7 +4,8 @@ export default class FirebaseService {
   _apiUrl = process.env.VUE_APP_URL
 
   async fetchData() {
-    const res = await axios.get(`${this.url}/tasks.json`)
+    const res = await axios.get(`${this._apiUrl}/tasks.json`)
+    if (!res.data) return false
     return Object.keys(res.data).map(key => {
       return {
         ...res.data[key],
@@ -13,11 +14,15 @@ export default class FirebaseService {
     })
   }
   async addTask(task) {
-    const res = await axios.post(`${process.env.VUE_APP_URL}/tasks.json`, task)
+    const res = await axios.post(`${this._apiUrl}/tasks.json`, task)
     return res.data.name
   }
   async deleteTask(id) {
-    const res = await axios.delete(`${process.env.VUE_APP_URL}/tasks.json`, id)
-    return res.data.name
+    const res = await axios.delete(`${this._apiUrl}/tasks/${id}.json`)
+    return res.status === 200 ? true : false
+  }
+  async updateTask(task) {
+    const res = await axios.put(`${this._apiUrl}/tasks/${task.id}.json`, task)
+    return res.status === 200 ? true : false
   }
 }
