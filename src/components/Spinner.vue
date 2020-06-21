fbafb7430108e1a6436b341f40afae38541b4cda
Rustam <mirzamorf@yandex.ru>
2020-06-22 02:19:18 +0300

    added new component Spinner

---
+++ src/components/Spinner.vue
@@ -0,0 +1,22 @@
+<template>
+  <div :class="{ 'spinner-large': isLarge }">
+    <div class="spinner-border text-success" role="status">
+      <span class="sr-only">Loading...</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    isLarge: Boolean
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.spinner-large {
+  margin: 200px auto;
+  text-align: center;
+}
+</style>
